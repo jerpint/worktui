9be8c6ce6346248e8d0eb9774a4848c8ef47265d
Jeremy Pinto <jerpint@gmail.com>
2026-02-14 17:29:38 -0500

    add vim keybindings

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,38 @@
-import { render, Text, Box, useInput, useApp } from "ink";
+import { Key, render, Text, Box, useInput, useApp } from "ink";
 import { useState } from "react";
 
+
 const items = ["Projects", "Timeline", "Stats", "Quit"];
+const vimUp = "k"
+const vimDown = "j"
+const vimLeft = "h"
+const vimRight = "l"
+
+function getInputDirection(input: string, key: Key): string | undefined {
+  if (key.upArrow || input == vimUp) {
+    return "up"
+  }
+  else if (key.downArrow || input == vimDown) {
+    return "down"
+  }
+  else if (key.leftArrow || input == vimLeft) {
+    return "left"
+  }
+  else if (key.rightArrow || input == vimRight) {
+    return "right"
+  }
+  else { return }
+}
 
 function App() {
   const [selected, setSelected] = useState(0);
   const { exit } = useApp();
 
   useInput((input, key) => {
-    if (key.upArrow) {
+    const dir = getInputDirection(input, key)
+    if (dir == "up") {
       setSelected((s) => (s > 0 ? s - 1 : items.length - 1));
-    } else if (key.downArrow) {
+    } else if (dir == "down") {
       setSelected((s) => (s < items.length - 1 ? s + 1 : 0));
     } else if (key.return) {
       if (items[selected] === "Quit") exit();
